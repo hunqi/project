5ced9567429d2cf819051b522dd04a9e50cd0505
raysun <leigeosxdi@163.com>
2017-07-25 17:54:23 +0800

    develop

---
+++ shirodemo-web/readme.txt
@@ -0,0 +1,4 @@
+代码参考：
+https://github.com/zhangkaitao/shiro-example/tree/master/shiro-example-chapter12
+
+这个应用是学习 张开涛<<跟我学shiro>>的练习
---
+++ shirodemo-web/src/main/java/com/raysun/shirodemo/web/mvc/ShiroDemoController.java
@@ -0,0 +1,24 @@
+package com.raysun.shirodemo.web.mvc;
+
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authz.annotation.RequiresRoles;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ShiroDemoController {
+	
+	@RequestMapping("/hello1")
+    public String hello1() {
+		System.out.println("begin checking privilege.");
+        SecurityUtils.getSubject().checkRole("admin");
+        return "success";
+    }
+
+    @RequiresRoles("admin")
+    @RequestMapping("/hello2")
+    public String hello2() {
+        return "success";
+    }
+    
+}
