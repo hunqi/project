682dd8f382f5355d545eff4cc72d1637d64ba0da
ray.sun <ray.sun@homecredit.cn>
2019-04-26 09:37:01 +0800

    remove redundant code

--- picwall/src/main/java/cn/rs/picwall/PicWallApplication.java
+++ picwall/src/main/java/cn/rs/picwall/PicWallApplication.java
@@ -5,8 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
-@PropertySource(value = {"classpath:application.properties",
-        "classpath:application-${spring.profiles.active}.properties"})
 public class PicWallApplication {
 
     public static void main(String[] args) {
